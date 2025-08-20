6d88553ffa0bb83d05c630e21cde46cc903c8dfe
jerome <jerome.dufour.inetum@externalgcp.ds.airbus.com>
2025-08-20 10:51:10 +0200

    feat: remove old event

--- src/app/home/event.ts
+++ src/app/home/event.ts
@@ -12,51 +12,6 @@ export interface Event {
   export const getEvents = (): Event[] => {
     return [
       {
-        title: 'CR DC',
-        start: '2025-02-15',
-        description: 'Championnat régional de DC 2025',
-        extendedProps: {
-          image: 'assets/img/CRDC_2025.png',
-        },
-        backgroundColor: 'red',
-      },
-      {
-        title: 'MQL',
-        start: '2025-03-15',
-        description: 'Main Qualifier Legacy 2025',
-        extendedProps: {
-          image: 'assets/img/MQL_2025.png',
-        },
-        backgroundColor: 'red',
-      },
-      {
-        title: '🧙 FNM Legacy',
-        start: '2025-04-11',
-        description: 'Avril FNM Legacy',
-        extendedProps: {
-          image: '',
-        },
-        backgroundColor: 'red',
-      },
-      {
-        title: '☀️ Eternal Sunday',
-        start: '2025-04-13',
-        description: 'Eternal Sunday Avril 2025, Peasant Vintage et Legacy',
-        extendedProps: {
-          image: 'assets/img/Eternal_sunday_04.jpg',
-        },
-        backgroundColor: 'red',
-      },
-      {
-        title: '🧙 FNM Legacy',
-        start: '2025-05-23',
-        description: 'Mai FNM Legacy',
-        extendedProps: {
-          image: '',
-        },
-        backgroundColor: 'red',
-      },
-      {
         title: '🌇 Pauper Morning',
         start: '2025-05-25',
         description: 'Pauper Eternal Morning',
